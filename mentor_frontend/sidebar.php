d62e506b5b0530d909cb71c8442e5783ffabf597
Devb1242 <devb1242@gmail.com>
2025-03-02 04:14:00 -0500

    Mentor Module Created

---
+++ mentor_frontend/sidebar.php
@@ -0,0 +1,13 @@
+<ul class="navbar-nav bg-gradient-info sidebar sidebar-dark accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+    <div class="sidebar-brand-icon">
+            <img src="Icons/samantah Johnson - wellness coaching - menthal help-1.png" alt="" style="width: 40px; height: 40px;">
+    </div>
+        <div class="sidebar-brand-text mx-3">HopeGuide</div>
+    </a>
+    <hr class="sidebar-divider my-0">
+    <li class="nav-item">
+        <a class="nav-link" href="chat.php"><i class="fas fa-comments"></i><span>Users</span></a>
+    </li>
+</ul>
+<!-- End of Sidebar -->
